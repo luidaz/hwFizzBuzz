9e00e76f0513ee8be32a250cfdf20e91b08782fe
luidaz <luidaz>
2019-04-01 11:04:18 +0300

    anotherHW

---
+++ hwFizzBuzz.py
@@ -0,0 +1,12 @@
+end = input("enter a number: ")
+end = int(end)
+
+for number in range(1, end+1):
+    if number % 3 == 0 and number % 5 == 0:
+        print('FizzBuzz')
+    elif number % 3 == 0:
+        print('Fizz')
+    elif number % 5 == 0:
+        print('Buzz')
+    else:
+        print(number)
